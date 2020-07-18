c3e97a710203fc17a8581ce7a0f6c7115824beec
极光05 <2405074757@qq.com>
2020-07-18 16:09:48 +0800

    更新了依赖

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter,BrowserRouter} from 'react-router-dom'; 
+// import {Provider} from 'react-redux';
+import store from './store'
 import App from './App';
 import './index.css';
 
