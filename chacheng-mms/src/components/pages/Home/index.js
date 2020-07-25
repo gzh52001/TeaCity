57a79fd2585aff1a4ec673dc267bc8e73cfa7b04
极光05 <2405074757@qq.com>
2020-07-25 09:23:32 +0800

    更新依赖

--- chacheng-mms/src/components/pages/Home/index.js
+++ chacheng-mms/src/components/pages/Home/index.js
@@ -8,7 +8,7 @@ class Home extends Component{
     componentDidMount(){}
     render(){
         return(
-            <div style={{width:'100%', height:'100%' ,display:'flex',justifyContent:'center',alignItems:'center', background:'url(./home_bg.jpg) no-repeat 0 0/100% 120%'}}>
+            <div style={{width:'100%', height:'100%' ,display:'flex',justifyContent:'center',alignItems:'center', background:'url(./img/home_bg.jpg) no-repeat 0 0/100% 120%'}}>
                 <h2 style={{fontSize:"26px",fontWeight:'bold'}}>欢迎使用醉品茶城后台管理系统！</h2>
             </div>
         )
